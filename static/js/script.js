65ccf93ff44ce8ab25d2d401723b99c66e6cac1b
minimouse0 <473147334@qq.com>
2024-11-29 01:26:48 +0800

    重构部分图片懒加载逻辑，实现高度计算部分也懒加载，节省前端性能

--- static/js/script.js
+++ static/js/script.js
@@ -1,14 +1,71 @@
 let isInitialRender = true;
 document.addEventListener("DOMContentLoaded", function () {
+    //<!--每groupSize张图片为一组来实现分区地对高度进行懒加载-->
+    const groupSize=100
+    const imageFileNameGroups=(()=>{
+        const result = [];
+        // 循环遍历原数组，每groupSize个为一组进行分割
+        for (let i = 0; i < imageFileNames.length; i += groupSize) {
+            result.push(imageFileNames.slice(i, i + groupSize));  // 使用slice提取每个片段并推入结果数组
+        }
+        return result;
+    })()
+
+    //步进渲染每一组
+    let renderGroupPointer=0;
+    /**每执行一次这个函数，都会自动步进一次 */
+    function renderNextGroup(){
+        if(renderGroupPointer>=imageFileNameGroups.length)return;
+        const imageFileNameGroup=imageFileNameGroups[renderGroupPointer]
+        const galleryContainer = document.getElementById('gallery-container');
+
+        for (let i in imageFileNameGroup) {
+            const imageFileName=imageFileNameGroup[i]
+            const imageCode = `
+            <div class="box">
+                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" 
+                    alt="${imageFileName}" 
+                    class="lazy-load" 
+                    data-src="/image/${imageFileName}" 
+                    data-original="/original/${imageFileName}" 
+                    data-groupid="${renderGroupPointer}" 
+                    data-grouporder="${i}" 
+                    onclick="openFullscreenImage(this)"
+                >
+            </div>
+            `;
+            
+            // 使用 innerHTML 追加内容到 <span> 容器
+            //imageGroupContainer.innerHTML += imageCode;
+
+            galleryContainer.innerHTML += imageCode
+        }
+        //新的元素加入后，要为新元素重新渲染，渲染完了才能懒加载
+        render()
+        //新的元素加入后，需要重新设置一遍懒加载的监听
+        refresh_lazy_load()
+        renderGroupPointer++;
+    }
+
+    for(let imageFileNameGroup of imageFileNameGroups){
+
+    }
+
+
+
+    
+
     // render();//不影响但是过度不一样（可注释）
     window.addEventListener("load", function () {
          // 仅在初始加载时执行布局调整
         if (isInitialRender) {
+            //开始时先渲染一组让屏幕上有图片，并且用这组来触发下一组的渲染
+            renderNextGroup()
 
             render();
             isInitialRender = false;
             //初始加载时，开始懒加载逻辑
-            start_lazy_load()
+            refresh_lazy_load()
         }
     });
     // 窗口大小改变布局调整
@@ -63,23 +120,26 @@ document.addEventListener("DOMContentLoaded", function () {
     //window.addEventListener("load", render);
     //window.addEventListener("resize", render);
 
-    function start_lazy_load(){
+    function refresh_lazy_load(){
         // 图片懒加载
         const lazyImages = document.querySelectorAll('.lazy-load');
         if ("IntersectionObserver" in window) {
             let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
-                entries.forEach(function (entry) {
-                    if (entry.isIntersecting) {
-                        let lazyImage = entry.target;
-                        lazyImage.src = lazyImage.dataset.src;
-                        lazyImage.onload = function () {
-                            // 图片加载完成后调用render函数重新计算布局
-                            render();
-                        };
-                        lazyImage.classList.remove('lazy-load');
-                        lazyImageObserver.unobserve(lazyImage);
+                entries.forEach(function (entry) {if (entry.isIntersecting) {
+                    //进入视口的是图片，渲染图片
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.onload = function () {
+                        // 图片加载完成后调用render函数重新计算布局
+                        render();
+                    };
+                    lazyImage.classList.remove('lazy-load');
+                    lazyImageObserver.unobserve(lazyImage);
+                    if(lazyImage.dataset.grouporder==groupSize-1){
+                        //最后一张图片露出来的时候加载下一组
+                        renderNextGroup()
                     }
-                });
+                }});
             });
 
             lazyImages.forEach(function (lazyImage) {
@@ -111,6 +171,7 @@ document.addEventListener("DOMContentLoaded", function () {
             window.location.href = this.getAttribute('href');
         });
     });
+
 });
 
 // 打开全屏图片
